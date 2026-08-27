b31fdfc13cd2a7de4d101b96eec42627303bb854
Chris Atwood <15620894+csatwood@users.noreply.github.com>
2026-08-27 07:12:27 -0700

    fix: align Node types and tooling ignores

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,5 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  ignores: ["coverage", "dist", "node_modules"],
+  ignores: ["coverage/**", "dist/**", "node_modules/**"],
 });
